69aeeb5dcbc5745748969376c02a6234468c9b77
Delvison Castillo <DelvisonCastillo@gmail.com>
2012-10-25 14:13:06 -0400

    Worked on Exam and Assignment

--- src/com/example/studentplanner/Assignment.java
+++ src/com/example/studentplanner/Assignment.java
@@ -1,30 +1,57 @@
+//Author: Delvison Castillo
+
 package com.example.studentplanner;
 
-import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Assignment {
   String name;
-  Calendar dueDate;
+  GregorianCalendar dueDate;
   String description;
   int pointsReceived;
   int maxPoints;
   int grade;
   boolean isComplete;
+  
   //have 2 constructors..one with or without maxpoints
-  public Assignment(String name, Calendar dueDate, String description, int maxPoints){
-	  //constructor
+  public Assignment(String name, GregorianCalendar dueDate, String description, int maxPoints){
+	  this.name = name;
+	  this.dueDate = dueDate;
+	  this.description = description;
+	  this.maxPoints = maxPoints;
   }
   
-  public void edit() {
-	  //ability to edit stuff here
-	  
+  public Assignment(String name, GregorianCalendar dueDate, String description){
+	  this.name = name;
+	  this.dueDate = dueDate;
+	  this.description = description;
   }
   
+  public void setName(String n) {
+	  this.name = n;
+  }
+  
+  public void setDueDate(GregorianCalendar c) {
+	  this.dueDate = c;
+  }
+  
+  public void setDescription(String d) {
+	  this.description = d;
+  }
+  
+  public void setPointsRecieved(int p) {
+	  this.pointsReceived = p;
+  }
+  
+  public void setMaxPoints(int m) {
+	  this.maxPoints = m;
+  }
+   
+  public void setIsComplete(boolean c) {
+	  this.isComplete = c;
+  }
+  
+  public void calculateGrade() {
+	  grade = ((pointsReceived)/(maxPoints));
+  }
 }
-
-/*
- * maybe have just name and date in the constructor
- * inputGrade() method that would take in pointsRecieved and maxPoints and calculate grade
-      ---> grade = (pointsRecieved / maxPoints)
- * edit() method in case things change
-*/
--- src/com/example/studentplanner/Exam.java
+++ src/com/example/studentplanner/Exam.java
@@ -1,24 +1,52 @@
+
 package com.example.studentplanner;
 
 import java.util.GregorianCalendar;
 
 public class Exam {
   String name;
-  GregorianCalendar Date;
+  GregorianCalendar dueDate;
   int pointsReceived;
   int maxPoints;
   int grade;
   boolean isComplete; // if checked, it wont show in masterview
   
   public Exam(String name, GregorianCalendar dueDate, int maxPoints) {
-	  //constructor
+	  this.name = name;
+	  this.dueDate = dueDate;
+	  this.maxPoints = maxPoints;
+  }
+  
+  public Exam(String name, GregorianCalendar dueDate) {
+	  this.name = name;
+	  this.dueDate = dueDate;
+  }
+  
+  public void setName(String n) {
+	  this.name = n;
+  }
+  
+  public void setdueDate(GregorianCalendar d) {
+	  this.dueDate = d;
+  }
+  
+  public void setPointsReceived(int p) {
+	  this.pointsReceived = p;
+  }
+  
+  public void setMaxPoints(int m) {
+	  this.maxPoints = m;
+  }
+  
+  public void setGrade(int g) {
+	  this.grade = g;
+  }
+  
+  public void setIsComplete(boolean c) {
+	  this.isComplete = c;
+  }
+  
+  public void calculateGrade() {
+	  this.grade = ((pointsReceived)/(maxPoints));
   }
 }
-
-/*
- * maybe have just name and date in the constructor
- * addGrade() method that would take in pointsRecieved and maxPoints and calculate grade
-      ---> grade = (pointsRecieved / maxPoints)
- * editExam() method in case things change
-*/
-
