c7573793d3269f5e5ccd85be34ae32d767f5dd62
bfamilo <bfamilo@oswego.edu>
2012-11-06 22:40:26 -0500

    CreateMiscActivity.java 	actually wrote it, comments need looking at MiscListviewActivity.java 	actually wrote it. MiscActivity.java 	actually wrote it, need to look at how we handle reoccurence, will bring up at meeting. Fixed some aspects of the views.
    Signed-off-by: bfamilo <bfamilo@oswego.edu>

--- src/com/example/studentplanner/CreateMiscActivity.java
+++ src/com/example/studentplanner/CreateMiscActivity.java
@@ -1,5 +1,87 @@
 package com.example.studentplanner;
 
-public class CreateMiscActivity {
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
+public class CreateMiscActivity extends Activity {
+Button createMisc;
+
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_misc);
+        createMisc = (Button) findViewById(R.id.createMisc);
+ 
+    }
+
+    public void handleClick1(View v){
+    	
+    	SQLiteDatabase db = openOrCreateDatabase("PlannerDB", MODE_PRIVATE, null);
+    	//db.execSQL("CREATE TABLE IF NOT EXISTS Courses (CourseName VARCHAR, Description VARCHAR," +
+    	//		" Location VARCHAR, HourStart INT, MinuteStart INT, YearStart INT, MonthStart INT, DayStart INT )");
+    	db.execSQL("CREATE TABLE IF NOT EXISTS Miscs (MiscName VARCHAR, Description VARCHAR," +
+    	    	" Location VARCHAR, HourStart INT, MinuteStart INT, YearStart INT, MonthStart INT, DayStart INT, " +
+    			" Occurences VARCHAR)");
+    	
+    	//extract miscName from view
+    	EditText editName = (EditText) findViewById(R.id.editName);
+    	String misc = (String) editName.getText().toString();
+    	
+    	//extract description from view
+    	EditText editDescription = (EditText) findViewById(R.id.editDescription);
+    	String descript = (String) editDescription.getText().toString();
+    	
+    	//extract location from view
+    	EditText editLocation = (EditText) findViewById(R.id.editLocation);
+    	String loc = (String) editLocation.getText().toString();
+    	
+    	//extract start date from view
+    	DatePicker datePicker1 = (DatePicker) findViewById(R.id.datePicker1);
+        int startDay = datePicker1.getDayOfMonth();
+        int startMonth = datePicker1.getMonth();
+        int startYear = datePicker1.getYear();
+            
+        //extract start time from view
+   	    TimePicker timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
+        int startMinute = timePicker1.getCurrentMinute(); //is this right?
+        int startHour = timePicker1.getCurrentHour(); //is this right?
+
+        //This really needs to be implemented in the Misc_view.xml
+        //Spinner reoccurSpinner = (Spinner) findViewById(R.id.reoccurSpinner);
+        //String occurences = (String) reoccurSpinner.getSelectedItem();
+        
+        ContentValues values = new ContentValues();
+        
+        values.put("MiscName", misc);
+        values.put("Description", descript);
+        values.put("Location", loc);
+        
+        values.put("YearStart", startYear);
+        values.put("MonthStart", startMonth);
+        values.put("DayStart", startDay);
+
+        values.put("HourStart", startHour);
+        values.put("MinuteStart", startMinute);
+        
+        values.put("Occurences", occurences);
+        
+        db.insert("Miscs", null, values);
+        
+        //db.execSQL("INSERT INTO Semesters VALUES(sessionName,startYear,startMonth,startDay,endYear,endMonth,endDay);");
+        db.close();		
+      
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        //putExtras() ... take user to the new course they just created
+        startActivity(intent);
+        
+    }
 }
---
+++ src/com/example/studentplanner/MiscListviewActivity.java
@@ -0,0 +1,107 @@
+package com.example.studentplanner;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MiscListviewActivity extends ListActivity {
+	String[] miscArr;
+	
+	   @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        try{
+	            //open database
+	    	    SQLiteDatabase db = openOrCreateDatabase("PlannerDB", MODE_PRIVATE, null);
+	    	    //query. receive a cursor
+	    	    Cursor c= db.rawQuery("SELECT MiscName FROM Miscs", null);
+	    	    //count how many items in cursor. add 1 to leave space for add semester option
+	    	    int i = c.getCount() + 1;
+	    	    //instantiate array of semesters by size of the cursor + 1
+	    	    miscArr = new String[i];
+	    	    //set up a count int to keep track of array positions
+	    	    int count = 0;
+	            //move the cursor to first position
+	    	    c.moveToFirst();
+	    	    
+	            //while the cursor position isn't passed the last item in the cursor 
+	    	    while(c.isAfterLast()==false) {
+	    	    		//store the string in "Session" column into the array of semesters
+	    	    		miscArr[count] = c.getString(c.getColumnIndex("MiscName"));
+	    	    		//increment count
+	    	    		count++;
+	    	    		//move cursor by 1
+	    	    		c.moveToNext();
+	    	    }
+	    	    //add an "add semester option
+	    	    miscArr[count] = new String("+Add Misc Event");
+	    	    //close the cursor
+	    	    c.close();
+	    	    //close the database
+	       	   	db.close();
+	       	   	//set a listadapter, use the semester_menu_view view and the semesterArr array
+	       	   	setListAdapter(new ArrayAdapter<String>(this, R.layout.semester_listview,miscArr));//needs an array
+	       	   	//get the list view from the view
+	       	   	ListView listView = getListView();
+	       	   	//set the listview's textfilter to enabled
+	       	   	listView.setTextFilterEnabled(true);
+	       	   	//add an onclicklistener
+	       	   	listView.setOnItemClickListener(new OnItemClickListener() {
+	    		
+	       	   		public void onItemClick(AdapterView<?> parent, View view,
+	       	   			int position, long id) {
+	       	   				//this string holds the list item clicked
+	       	   				String rightMeow = miscArr[position];
+
+	       	   				// When clicked, show a toast with the TextView text
+	       	   				//Toast.makeText(getApplicationContext(),
+	       	   				//((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+	       	   				//if item clicked equals add semester
+	       	   				
+	       	   				if (rightMeow.equals("+Add Misc Event")){  
+	       	   					Intent intent = new Intent(getApplicationContext(), CreateMiscActivity.class);
+	       	   					//then go to the CreateSemesterActivity
+	       	   					startActivity(intent);
+	       	   				} else {
+	       	   					Intent intent = new Intent(getApplicationContext(), CourseActivity.class);
+	       	   					intent.putExtra("key", rightMeow);
+	       	   					startActivity(intent);
+	       	   				}
+	       	   		}
+	       	   	}); //close listener
+	       	  
+	        }catch(SQLiteException e){
+	            // Create the alert box
+	           AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+
+	           // Set the message to display
+	           alertbox.setMessage("Welcome! To get started please click on Add Misc Event!");
+
+	           // Add a neutral button to the alert box and assign a click listener
+	           alertbox.setNeutralButton("Add Misc Event", new DialogInterface.OnClickListener() {
+
+	               // Click listener on the neutral button of alert box
+	               public void onClick(DialogInterface arg0, int arg1) {
+
+	                   // The neutral button was clicked
+	            	   Intent intent = new Intent(getApplicationContext(), CreateMiscActivity.class);
+	                   startActivity(intent);
+	               }
+	           });
+
+	            // show the alert box
+	           alertbox.show();
+	       }
+	        
+	    }//end of onCreate()
+}
